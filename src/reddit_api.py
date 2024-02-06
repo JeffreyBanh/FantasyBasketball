747a2ebc5b813d4858384c1c0eb1421e5d47109e
Michael Lin <michaelwlin@icloud.com>
2024-02-05 21:11:39 -0800

    changes for args and cred

--- src/reddit_api.py
+++ src/reddit_api.py
@@ -2,6 +2,7 @@ from data import *
 import requests
 import datetime
 import praw
+import argparse
 
 def getRedditAPI():
     # CLIENT_ID = 'eDX7DeW85uiRe6UJNCvl-A'
@@ -20,11 +21,11 @@ def getRedditAPI():
     # headers['Authorization'] = f'bearer {TOKEN}'
 
     reddit = praw.Reddit(
-        client_id="eDX7DeW85uiRe6UJNCvl-A",
-        client_secret="tnlVQwNyWEMpQmQuWr8_OS4NV1uUZQ",
-        password="Zsaber123!",
-        user_agent="Fantasybball user agent",
-        username="clowob",
+        client_id="I2p_CGlYpfzkRVIgB7Grrg",
+        client_secret="_kB2X6TYbMEtOrM3X-PJeGw_OJdH0g",
+        password="asdlkjf192849asd",
+        user_agent="fantasy_sports_stats",
+        username="kdeezburner",
     )
     return reddit
 
@@ -55,11 +56,12 @@ def textBestOutput(values, columns):
 
 def submitReddit(reddit, subreddit, title, text):
     subreddit = reddit.subreddit(subreddit)
-    # choices = list(subreddit.flair.link_templates.user_selectable())
-    # template_id = next(x for x in choices if x["flair_text"] == "Discussion")["flair_template_id"]
-    # res = subreddit.submit(title = title, selftext = text, flair_id = template_id)
+    choices = list(subreddit.flair.link_templates.user_selectable())
+    template_id = next(x for x in choices if x["flair_text"] == "Discussion")["flair_template_id"]
+    res = subreddit.submit(title = title, selftext = text, flair_id = template_id)
+    print(res)
     # # subreddit.submit("title", flair_id=template_id, url="https://www.news.com/")
-    subreddit.submit(title = title, selftext = text)
+    #subreddit.submit(title = title, selftext = text)
     # print(subreddit.name)
     # resGet = requests.get('https://oauth.reddit.com/r/fantasybball/about')
     # res = requests.post('https://oauth.reddit.com/api/submit', headers = headers, data = json.dumps(obj))
@@ -67,25 +69,66 @@ def submitReddit(reddit, subreddit, title, text):
     # print(res.json())
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-dr',
+        '--dryrun',
+        default='1'
+    )         
+    parser.add_argument(
+        '-ot',
+        '--output_type',
+        help="Output type to use. ie: -ot 'best' | -ot 'worst' | -ot 'both'",
+        default='worst'
+    )
+    args = parser.parse_args()
+    dryrun = int(args.dryrun)
+    output_type = args.output_type
+
+
+    # Get reddit obj
     reddit = getRedditAPI()
+    # Parse data
     data, list_header = getData()
+    
+    # Format data
     df = createDataFrame(data, list_header)
     worstPlayersDF = getWorstPlayersValue(df)
-    bestPlayersDF = getBestPlayers(df)
     worstValues = worstPlayersDF.values.tolist()
     worstColumns = worstPlayersDF.columns.tolist()
+    
+    bestPlayersDF = getBestPlayers(df)
     bestValues = bestPlayersDF.values.tolist()
     bestColumns = bestPlayersDF.columns.tolist()
+    
+    # Outputs
     worstOutput = textWorseOutput(worstValues, worstColumns)
+    bestOutput = textBestOutput(bestValues, bestColumns)
+
+    # Get date
     today = datetime.date.today()
     d2 = today.strftime("%B %d, %Y")
+
+    # Format titles
     worstTitle = "Top 10 Worst Performances of the Night 🤢 {} Night 🤮 ({})".format(worstValues[0][1], d2)
     bestTitle = "Top 10 Performances of the Night 🥹 {} Night 🙌 ({})".format(bestValues[0][1], d2)
 
-    bestOutput = textBestOutput(bestValues, bestColumns)
-    # submitReddit(reddit, 'testingTables', bestTitle, bestOutput)
-    print(worstPlayersDF)
-    print(worstOutput)
-    print(worstTitle)
+    if output_type == 'worst':
+        if dryrun:
+            print(worstOutput)
+        else:
+            submitReddit(reddit, 'fantasybball', worstTitle, worstOutput)
+    elif output_type == 'best':
+        if dryrun:
+            print(bestOutput)
+        else:
+            pass
+            #submitReddit(reddit, 'fantasybball', bestTitle, bestOutput)
+    elif output_type == 'both':
+        pass
+    else:
+        raise SystemExit("-ot {} not valid".format(output_type))
 
-main()
+
+if __name__ == "__main__":
+    main()
